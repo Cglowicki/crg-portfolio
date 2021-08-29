a5dca1012191dee0b4276786fd13f0ad60a6e2e1
Cam <camglowicki@hotmail.com>
2021-08-29 12:06:11 -0700

    rid of nav component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import Nav from './components/Nav.jsx';
+
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <h1>
-        CRG Portfolio
-      </h1>
     </div>
   );
 }
--- src/components/Nav.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Nav.scss';
-
-export default function Nav() {
-  return (
-    <nav className="nav-menu">Cameron Glowicki</nav>
-  )
-}
