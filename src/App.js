7d9354a984cf35ce48676e1ec465567c98d9e5c9
Cam <camglowicki@hotmail.com>
2021-08-29 13:37:14 -0700

    size intro-slide box

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import IntroDisplay from './components/IntroDisplay';
 function App() {
   return (
     <div className="App">
+      <h1 className="name">Cameron Glowicki</h1>
       <IntroDisplay />
     </div>
   );
--- src/components/IntroDisplay.jsx
+++ src/components/IntroDisplay.jsx
@@ -4,7 +4,6 @@ import './IntroDisplay.scss';
 export default function IntroDisplay() {
   return (
     <div className="intro-slide">
-      <h1 className="name">Cameron Glowicki</h1>
     </div>
   );
 };
